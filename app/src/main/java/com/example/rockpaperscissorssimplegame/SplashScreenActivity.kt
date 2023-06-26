86531ae2acdefefe8368a2de172f2bd72458556c
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-06-26 21:19:06 +0700

    successfully create logic, layout on game activity and then successfully create localization for Indonesian language

--- app/src/main/java/com/example/rockpaperscissorssimplegame/SplashScreenActivity.kt
+++ app/src/main/java/com/example/rockpaperscissorssimplegame/SplashScreenActivity.kt
@@ -18,7 +18,7 @@ class SplashScreenActivity : AppCompatActivity() {
         )
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, GameActivity::class.java)
             startActivity(intent)
             finish()
         },3000)
